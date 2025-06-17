8448e6a1143361345655eb5ce478b68d9b784262
Joshua <joshuawoolman15@gmail.com>
2025-06-17 06:22:33 -0400

    steps change with group

---
+++ resources/res_step_group.gd
@@ -0,0 +1,4 @@
+class_name StepGroup extends Resource
+
+@export var group: String
+@export var streams: AudioStream
--- scripts/player/step_player.gd
+++ scripts/player/step_player.gd
@@ -1,11 +1,12 @@
 extends AudioStreamPlayer3D
 
 @export var player: CharacterBody3D
+@export var step_groups: Array[StepGroup]
 
 var step_timer: Timer = Timer.new()
 var ray: RayCast3D = RayCast3D.new()
+var current_group: StepGroup
 
-var A
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,11 +21,23 @@ func _process(delta: float) -> void:
 	if player.velocity.length() > 0 and player.is_on_floor() && step_timer.is_stopped(): 
 		step_timer.start()
 	
-	if player.velocity.length() <= 0 and !step_timer.is_stopped(): 
+	if player.velocity.length() <= 0 and !step_timer.is_stopped() or !player.is_on_floor(): 
 		step_timer.stop()
 
 	if ray.is_colliding(): 
 		var groups = ray.get_collider().get_groups()
+	setup_groups()
+
+
+func setup_groups(): 
+	if !ray.is_colliding(): return 
+	
+	var groups = ray.get_collider().get_groups()
+	for group in groups: 
+		for step in step_groups: 
+			if group == step.group and current_group != step: 
+				current_group = step
+				stream = current_group.streams
 
 
 func _timeout(): 
